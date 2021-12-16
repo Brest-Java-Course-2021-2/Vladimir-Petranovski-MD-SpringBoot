6c7945500ab6dee2020691538bf846a31a02239f
avp02 <vpetranovskij@gmail.com>
2021-12-16 19:21:58 +0300

    added service-api module

---
+++ service-api/src/main/java/com/epam/brest/service_api/CarService.java
@@ -0,0 +1,60 @@
+package com.epam.brest.service_api;
+
+import com.epam.brest.model.Car;
+
+import java.util.List;
+
+public interface CarService {
+
+    /**
+     * Find all cars.
+     *
+     * @return list of cars.
+     */
+
+    List<Car> findAllCars();
+
+    /**
+     * Find car by Id.
+     *
+     * @param id car Id.
+     * @return car.
+     */
+
+    Car findCarById(Integer id);
+
+    /**
+     * Persist new car.
+     *
+     * @param car car.
+     * @return persisted car id.
+     */
+
+    Integer saveCar(Car car);
+
+    /**
+     * Update car.
+     *
+     * @param id car id.
+     * @param car car.
+     * @return number of updated records in the database.
+     */
+
+    Integer updateCarById(Integer id, Car car);
+
+    /**
+     * Delete car.
+     *
+     * @param id car id.
+     * @return number of updated records in the database.
+     */
+    Integer deleteCarById(Integer id);
+
+    /**
+     * Count cars.
+     *
+     * @return quantity of the cars.
+     */
+
+    Integer count();
+}
---
+++ service-api/src/main/java/com/epam/brest/service_api/DriverService.java
@@ -0,0 +1,61 @@
+package com.epam.brest.service_api;
+
+import com.epam.brest.model.Driver;
+
+import java.util.List;
+
+public interface DriverService {
+
+    /**
+     * Find all drivers.
+     *
+     * @return list of drivers.
+     */
+
+    List<Driver> findAllDrivers();
+
+    /**
+     * Find driver by Id.
+     *
+     * @param id driver Id.
+     * @return driver.
+     */
+
+    Driver findDriverById(Integer id);
+
+    /**
+     * Persist new driver.
+     *
+     * @param driver driver.
+     * @return persisted driver id.
+     */
+
+    Integer saveDriver(Driver driver);
+
+    /**
+     * Update department.
+     *
+     * @param id driver id.
+     * @param driver driver.
+     * @return number of updated records in the database.
+     */
+
+    Integer updateDriverById(Integer id, Driver driver);
+
+    /**
+     * Delete driver.
+     *
+     * @param id driver id.
+     * @return number of updated records in the database.
+     */
+
+    Integer deleteDriverById(Integer id);
+
+    /**
+     * Count drivers.
+     *
+     * @return quantity of the drivers.
+     */
+
+    Integer count();
+}
